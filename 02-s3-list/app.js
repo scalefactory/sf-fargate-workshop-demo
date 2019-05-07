0763c2d66d764fda8e13b247809c3fdb4a19c78f
Steve Porter <12199424+StevePorter92@users.noreply.github.com>
2019-05-07 17:28:10 +0100

    Update logging formats

--- 02-s3-list/app.js
+++ 02-s3-list/app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+app.use(logger('common'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
